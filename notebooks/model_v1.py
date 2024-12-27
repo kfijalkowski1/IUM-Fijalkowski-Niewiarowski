8b0232737bcc22831bc56acc4d002ce97a433b60
bartek <bniewiar>
2024-12-27 22:12:45 +0100

    move files

--- notebooks/model_v1.py
+++ notebooks/model_v1.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import joblib
 
 # Wczytanie danych
 file_path = './data_analize_scripts/dane/v1/updated_sessions.jsonl'  # Podmień na właściwą ścieżkę
@@ -19,10 +20,21 @@ data = data[(data['duration_ms'] > 0) &
             (data['avg_percent_played'] >= 0) & (data['avg_percent_played'] <= 100) &
             (data['percent_completed'] >= 0) & (data['percent_completed'] <= 100)]
 
+# Zrównoważenie danych
+# Filtracja przypadków 100%
+data_100 = data[data['percent_played'] == 100]
+data_other = data[data['percent_played'] < 100]
+
+# Zmniejszenie liczby przypadków 100%
+balanced_data_100 = data_100.sample(frac=0.1, random_state=42)  # Zachowujemy tylko 10% przypadków 100%
+
+# Połączenie zrównoważonych danych
+data_balanced = pd.concat([data_other, balanced_data_100])
+
 # Tworzenie cech wejściowych
 features = ['duration_ms', 'duration_time', 'avg_percent_played', 'percent_completed']
-X = data[features]
-y = data['percent_played']
+X = data_balanced[features]
+y = data_balanced['percent_played']
 
 # Skalowanie danych
 scaler = StandardScaler()
@@ -43,6 +55,10 @@ rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 mae = mean_absolute_error(y_test, y_pred)
 print(f"RMSE: {rmse:.2f}")
 print(f"MAE: {mae:.2f}")
+
+model_file_path = './models/v1/gradient_boosting_regressor_undersampling.pkl'
+joblib.dump(model, model_file_path)
+print(f"Model zapisano do pliku: {model_file_path}")
 
 # Przykładowe wyniki
 print("Przykładowe przewidywania:")
